65fef8bc704d7b1d9249a7c87c4cda333de737dc
Sayan27112000 <roy.sayan0197@gmail.com>
2022-06-01 13:22:33 +0530

    Error Updates

--- todo_list/src/environments/environment.prod.ts
+++ todo_list/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production : false,
+  firebase : {
+    apiKey: "AIzaSyCHhMLQLHahozqyFHDDXhgWJXsZXiSsYeM",
+    authDomain: "todo-list-application-6dcb8.firebaseapp.com",
+    projectId: "todo-list-application-6dcb8",
+    storageBucket: "todo-list-application-6dcb8.appspot.com",
+    messagingSenderId: "747107371450",
+    appId: "1:747107371450:web:c2c90f83a9d85425f3f583",
+    measurementId: "G-NHG6XYDB9L"
+  }
 };
